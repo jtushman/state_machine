2086406a56a81bc1d5bab36467e2fec30e7a4223
Jonathan Tushman <jtushman@pipewave.com>
2014-03-19 15:21:42 -0400

    fixing setup packages issue

--- setup.py
+++ setup.py
@@ -1,9 +1,20 @@
+import os
 from setuptools import setup
+
+
+def get_packages():
+    # setuptools can't do the job :(
+    packages = []
+    for root, dirnames, filenames in os.walk('state_machine'):
+        if '__init__.py' in filenames:
+            packages.append(".".join(os.path.split(root)).strip("."))
+
+    return packages
 
 required_modules = []
 
 setup(name='state_machine',
-      version='0.2.3',
+      version='0.2.4',
       description='Python State Machines for Humans',
       url='http://github.com/jtushman/state_machine',
       author='Jonathan Tushman',
