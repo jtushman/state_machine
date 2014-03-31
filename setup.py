de2a501d837a300dbf848309964ca19cf6601b3f
Jonathan Tushman <jtushman@pipewave.com>
2014-03-31 07:52:01 -0400

    bump

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def get_packages():
 required_modules = []
 
 setup(name='state_machine',
-      version='0.2.8',
+      version='0.2.9',
       description='Python State Machines for Humans',
       url='http://github.com/jtushman/state_machine',
       author='Jonathan Tushman',
